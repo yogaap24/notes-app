011e0cb61ac1f4121484193cda4301c0cd089491
Yoga Agung Prasetio <yoga.agung@transtrack.id>
2023-10-10 03:27:00 +0700

    collaboration service 2

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -46,8 +46,7 @@ class NotesHandler {
     const { id } = request.params;
     const { id: credentialId } = request.auth.credentials;
 
-    await this._service.verifyNoteOwner(id, credentialId);
-
+    await this._service.verifyNoteAccess(id, credentialId);
     const note = await this._service.getNoteById(id);
 
     const response = h.response({
@@ -66,7 +65,7 @@ class NotesHandler {
     const { id } = request.params;
     const { id: credentialId } = request.auth.credentials;
 
-    await this._service.verifyNoteOwner(id, credentialId);
+    await this._service.verifyNoteAccess(id, credentialId);
     await this._service.editNoteById(id, request.payload);
 
     const response = h.response({
