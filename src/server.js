4428162ba7676d52a2d1e81deecda03aac588d4a
Yoga Agung Prasetio <yoga.agung@transtrack.id>
2023-11-07 20:58:39 +0700

    S3 storage implemented

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ const ExportsValidation = require('./validation/exports');
 
 // uploads
 const uploads = require('./api/uploads');
-const StorageService = require('./services/storage/StorageService');
+const StorageService = require('./services/s3/StorageService');
 const UploadsValidation = require('./validation/uploads');
 
 const init = async () => {
@@ -43,7 +43,7 @@ const init = async () => {
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
-  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images'));
+  const storageService = new StorageService();
 
   const server = Hapi.server({
     port: process.env.PORT,
